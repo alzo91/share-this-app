8273071999515110f60bf54bb17092e4c72596b8
Alisson <alzo.zotarelli@gmail.com>
2023-04-01 10:25:45 -0300

    feat: Installed and configured react-native-paper

--- src/components/atoms/Label/styles.ts
+++ src/components/atoms/Label/styles.ts
@@ -1,9 +1,11 @@
 import styled from "styled-components/native";
+import { Text as Label } from "react-native-paper";
 
 interface TextProps {
   isLight?: boolean;
 }
-export const Text = styled.Text<TextProps>`
+
+export const Text = styled(Label)<TextProps>`
   font-family: ${(props) => props.theme.FONTS.REGULAR};
   font-size: 12px;
   color: ${(props) =>
--- src/components/atoms/Title/index.tsx
+++ src/components/atoms/Title/index.tsx
@@ -1,28 +1,28 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { useTheme } from "styled-components/native";
-
+import { Title as Label } from "react-native-paper";
 interface TitleProps {
   text: string;
   isLight?: boolean;
   testID?: string;
 }
+
 const Title: React.FC<TitleProps> = ({ text, isLight, testID }) => {
   const { COLORS, FONTS } = useTheme();
 
-  const styles = StyleSheet.create({
-    title: {
-      fontFamily: FONTS.SEMI_BOLD,
-      color: isLight ? COLORS.WHITE : COLORS.TERTIARY,
-      fontSize: 22,
-    },
-  });
-
   return (
     <View testID={`title-container-${testID}`}>
-      <Text testID={`title-text-${testID}`} style={styles.title}>
+      <Label
+        testID={`title-text-${testID}`}
+        style={{
+          fontFamily: FONTS.SEMI_BOLD,
+          color: isLight ? COLORS.WHITE : COLORS.TERTIARY,
+          fontSize: 22,
+        }}
+      >
         {text}
-      </Text>
+      </Label>
     </View>
   );
 };
