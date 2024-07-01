7f3c4e5a4522561bb9db93fbc6d489b4d162505f
Mac mini Ativy <alisson.oliveira@ativy.com>
2024-07-01 10:39:14 -0300

    update: Updating dependences

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { registerRootComponent } from "expo";
+import { LogBox } from "react-native";
 
 import App from "./src";
 
+LogBox.ignoreLogs(["TextInput.Icon: Support for defaultProps "]);
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
